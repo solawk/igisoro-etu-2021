a477fcb5f7516c471dda62625786c9987f3a73db
Alexander <37877585+solawk@users.noreply.github.com>
2021-05-07 20:00:07 +0300

    remove port from wss link

--- client.js
+++ client.js
@@ -36,7 +36,7 @@ export function ConnectToServer()
     Subject.Notify("serverPend");
     console.log("Connection pending");
 
-    serverWebsocket = new WebSocket("wss://igisoro.herokuapp.com:5000");
+    serverWebsocket = new WebSocket("wss://igisoro.herokuapp.com");
 
     serverWebsocket.addEventListener("open", function()
     {
